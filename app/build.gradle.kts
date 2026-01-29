07f927a283356d10f63cf8144a97a7744acd28f4
Navneet Kaur <navneetkaur131995@gmail.com>
2026-01-29 16:16:42 -0500

    Updated gradle(app) before creating multi-module gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.hilt.android)
-    id("kotlin-kapt")
 }
 
 android {
@@ -45,24 +44,46 @@ android {
 }
 
 dependencies {
+    // Android Lifecycle
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Jetpack compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Paging with compose
+    implementation(libs.androidx.compose.paging)
+    implementation(libs.androidx.compose.paging.runtime)
+
+    // Room
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
     testImplementation(libs.room.testing)
+
+    // Hilt compose navigation
+    implementation(libs.androidx.hilt.navigation)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
+
+    // Hilt
     implementation(libs.android.hilt)
-    kapt(libs.android.hilt.compiler)
+    ksp(libs.android.hilt.compiler)
+
+    // Kotlin Coroutines
     implementation(libs.kotlin.coroutines)
+
+    // Retrofit + OKHttp
     implementation(libs.squareup.retrofit)
     implementation(platform(libs.squareup.okhttp.bom))
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.retrofit.gson.converter)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
