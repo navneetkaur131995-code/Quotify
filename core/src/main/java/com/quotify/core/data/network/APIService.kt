f13bcc4ec5f5dcb33f5c2cae3a1c265e9bdfaff8
Navneet Kaur <navneetkaur131995@gmail.com>
2026-05-13 13:23:54 -0400

    Code Reviewed & Updated

--- core/src/main/java/com/quotify/core/data/network/APIService.kt
+++ core/src/main/java/com/quotify/core/data/network/APIService.kt
@@ -1,9 +1,7 @@
 package com.quotify.core.data.network
 
-import com.quotify.core.data.model.QuoteAPIResponse
 import com.quotify.core.data.model.QuotesListAPIResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIService {
